bcc1ace691866ec7407f6857537fca68c6c7ba2f
MH <mhemail12345@gmail.com>
2024-10-26 13:33:19 +0700

    new login and signup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("com.hbb20:ccp:2.7.0")
     //volley
     implementation(libs.volley)
     testImplementation(libs.junit)
---
+++ app/src/main/java/com/example/ideality/activities/SignUp.kt
@@ -0,0 +1,30 @@
+package com.example.ideality.activities
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ideality.R
+import com.hbb20.CountryCodePicker
+
+class SignUp : AppCompatActivity() {
+    private lateinit var ccp: CountryCodePicker
+    private lateinit var phoneInput: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        // Initialize views
+        ccp = findViewById(R.id.ccp)
+        phoneInput = findViewById(R.id.phoneInput)
+
+        // Register the phone number field with CCP
+        ccp.registerCarrierNumberEditText(phoneInput)
+
+
+        }
+    }
+}
